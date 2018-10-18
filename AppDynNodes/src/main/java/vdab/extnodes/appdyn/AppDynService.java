b877eb30be56ea030de76c64ce99dd82bd956bfd
markjapp <markjapp@gmail.com>
2018-10-18 14:30:28 -0400

    Added Apache licens

--- AppDynNodes/src/main/java/vdab/extnodes/appdyn/AppDynService.java
+++ AppDynNodes/src/main/java/vdab/extnodes/appdyn/AppDynService.java
@@ -1,15 +1,31 @@
+/*LICENSE*
+ * Copyright (C) 2013 - 2018 MJA Technology LLC 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 package vdab.extnodes.appdyn;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import vdab.api.node.HTTPService_A;
+
 import com.lcrc.af.AnalysisData;
 import com.lcrc.af.AnalysisEvent;
 import com.lcrc.af.constants.IconCategory;
-import com.lcrc.af.service.HTTPService;
 
 
-public class AppDynService extends  HTTPService  {
+public class AppDynService extends  HTTPService_A  {
 
 	private String c_Host = "localhost";
 	private Integer c_Port =  Integer.valueOf(8293);
